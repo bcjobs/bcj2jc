6720007bc8b88eab0eaf6b89ce48bdc3f4f139e6
Dmitry Nogin <dmitryn@mail.com>
2019-04-04 23:42:57 -0700

    Read credentials from the command line parameters

--- Bcj2jc/Program.cs
+++ Bcj2jc/Program.cs
@@ -11,12 +11,12 @@ namespace Bcj2jc
 {
     class Program
     {
+        // bcj2jc "feed-url" "sql-conn-str"
         static void Main(string[] args) => 
             AsyncContext.Run(() => 
                 MainAsync(
-                    new BCJobsFeed("file:///c:/proj/bcj2jc/Bcj2jc.Tests/bin/Debug/netcoreapp2.1/Feed.xml"),
-                    //new BCJobsFeed("https://www.bcjobs.ca/rss/jobs.xml?source=Jobcast&medium=referral&campaign=SearchEngines"),
-                    new JobcastDb("Data Source=DIMA-WS;Integrated Security=True;Database=JobcastDev;MultipleActiveResultSets=True")));
+                    new BCJobsFeed(args[0]),
+                    new JobcastDb(args[1])));
 
         static async Task MainAsync(IJobFeed feed, IJobDb db)
         {
